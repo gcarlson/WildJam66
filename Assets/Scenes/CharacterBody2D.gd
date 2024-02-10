68bca0b02ca7c96f5b6e1662ac11a777fa6eb392
gcarlson <grantcarlson6@gmail.com>
2024-02-10 00:47:10 -0500

    Added sprites to player.

--- Assets/Scenes/CharacterBody2D.gd
+++ Assets/Scenes/CharacterBody2D.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
+@onready var big_sprite = $BigSprite
+@onready var small_sprite = $SmallSprite
+@onready var big_collider = $BigCollider
+@onready var small_collider = $SmallCollider
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+var armored = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,12 +22,21 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	if Input.is_action_just_pressed("switch_form"):
+		armored = not armored
+		big_sprite.visible = armored
+		big_collider.disabled = not armored
+		small_sprite.visible = not armored
+		small_collider.disabled = armored			
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		big_sprite.flip_h = (direction < 0)
+		small_sprite.flip_h = (direction < 0)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
