9cef638dc18f29224a5f3062e96f5bed2073b950
gcarlson <grantcarlson6@gmail.com>
2024-02-10 14:39:50 -0500

    Added animated sprite to player and made player its own scene.

--- Assets/Scenes/CharacterBody2D.gd
+++ Assets/Scenes/CharacterBody2D.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@onready var big_sprite = $BigSprite
+@onready var big_sprite = $BigAnimatedSprite
 @onready var small_sprite = $SmallSprite
 @onready var big_collider = $BigCollider
 @onready var small_collider = $SmallCollider
@@ -9,6 +9,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 var armored = true
+var active_sprite = big_sprite
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -25,10 +26,16 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_pressed("switch_form"):
 		armored = not armored
+		
 		big_sprite.visible = armored
 		big_collider.disabled = not armored
 		small_sprite.visible = not armored
-		small_collider.disabled = armored			
+		small_collider.disabled = armored
+		
+		if armored:
+			active_sprite = big_sprite
+		else:
+			active_sprite = small_sprite
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -37,7 +44,9 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 		big_sprite.flip_h = (direction < 0)
 		small_sprite.flip_h = (direction < 0)
+		big_sprite.play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		big_sprite.play("Walk")
 
 	move_and_slide()
