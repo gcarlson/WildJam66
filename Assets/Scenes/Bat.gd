6d1bf878fa91ea223219b34b4b3fc5fdfaa338e2
gcarlson <grantcarlson6@gmail.com>
2024-02-16 22:36:23 -0500

    Added main menu and locked aspect ratio to prevent camera weirdness

--- Assets/Scenes/Bat.gd
+++ Assets/Scenes/Bat.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var navAgent = $NavigationAgent2D
+@onready var sprite = $AnimatedSprite2D
 
 const SPEED = 60
 const JUMP_VELOCITY = -400.0
@@ -12,19 +13,15 @@ var player
 var aggroed = false
 
 var facing_right = true
-var tunnel_cooldown = 5
-var tunnel_dur = -1
-var tunnel_target
-
 
 func _ready():
-	print("ddd init")
 	player = get_tree().get_first_node_in_group("Player")
 
 func _physics_process(delta):
 	if aggroed and not player.safe:
 		navAgent.target_position = player.global_position + Vector2(0, 0.0 if player.armored else 24.5)
 		velocity = (navAgent.get_next_path_position() - global_position).normalized() * SPEED
+		sprite.flip_h = (velocity.x > 0)
 		move_and_slide()
 
 func _on_area_2d_body_entered(body):
---
+++ Assets/Scenes/home_page.gd
@@ -0,0 +1,40 @@
+extends CanvasLayer
+
+
+@onready var buttons = [$RichTextLabel2, $RichTextLabel3, $RichTextLabel4]
+@onready var level_menu = $"../LevelMenu"
+
+var scenes = ["res://Assets/Scenes/level_2.tscn", "res://Assets/Scenes/level_1.tscn", "res://Assets/Scenes/level_3.tscn"]
+
+var index = 0
+
+var move_cooldown = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if not visible:
+		return
+	if Input.is_action_just_pressed("ui_accept"):
+		if index == 0:
+			get_tree().change_scene_to_file(scenes[0])
+		elif index == 1:
+			await get_tree().create_timer(0.1).timeout
+			level_menu.visible = true
+			visible = false
+	else:
+		move_cooldown -= delta
+		var direction = Input.get_axis("ui_up", "ui_down")
+		if direction and move_cooldown <= 0:
+			var s = buttons[index].text
+			print(len(s))
+			print("substr ", s.substr(9, len(s) - 10))
+			buttons[index].text = "[center]" + s.substr(9, len(s) - 10)
+			index = (index + (1 if direction > 0 else -1) + 3) % 3
+			s = buttons[index].text
+			buttons[index].text = "[center]>" + s.substr(8, len(s) - 1) + "<"
+			move_cooldown = 0.5
