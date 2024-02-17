36c9cb7fa9abbfd1d82997f9b61d6b357c608bed
gcarlson <grantcarlson6@gmail.com>
2024-02-17 01:35:52 -0500

    Made door slide up

--- Assets/Scenes/Door.gd
+++ Assets/Scenes/Door.gd
@@ -5,8 +5,15 @@ extends StaticBody2D
 @onready var occluder = $LightOccluder2D
 @onready var collider = $CollisionShape2D
 
+var closed = true
+var open_time = 0.0
+
 func open():
-	sprite.play("open")
-	occluder.visible = false
-	collider.set_deferred("disabled", true)
+	if closed:
+		closed = false
+		open_time = 1.0
 	
+func _physics_process(delta):
+	if open_time > 0:
+		open_time -= delta
+		position.y -= delta * 48
